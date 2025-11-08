a7700e4de4c2625dffd0ff4589bce9a191fd9e40
Rodrigo Garcia <garciarodrigov00@gmail.com>
2025-11-08 17:58:17 -0300

    version base home y form

---
+++ js/formularios.js
@@ -0,0 +1,34 @@
+// Detectar parámetro ?tipo=ayuda o ?tipo=voluntario
+document.addEventListener("DOMContentLoaded", () => {
+  const params = new URLSearchParams(window.location.search);
+  const tipo = params.get("tipo");
+
+  const formAyuda = document.getElementById("formAyuda");
+  const formVoluntario = document.getElementById("formVoluntario");
+
+  if (tipo === "ayuda") {
+    formAyuda.style.display = "block";
+  } else if (tipo === "voluntario") {
+    formVoluntario.style.display = "block";
+  }
+});
+
+function volverInicio() {
+  window.location.href = "index.html";
+}
+
+const fileInput = document.getElementById('fileInput');
+if (fileInput) {
+  fileInput.addEventListener('change', () => {
+    const fileName = document.getElementById('fileName');
+    fileName.textContent = fileInput.files.length ? fileInput.files[0].name : '';
+  });
+}
+
+function abrirLogin() {
+  window.location.href = "index.html?abrirLogin=true";
+}
+
+function loginConPami() {
+  window.location.href = "https://login.mi.pami.org.ar/login?authRequest=V2_345823951265528786";
+}
---
+++ js/main.js
@@ -0,0 +1,59 @@
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+// Modal de accion ingresar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+function openLoginModal() {
+  document.getElementById("loginModal").style.display = "block";
+}
+
+function closeLoginModal() {
+  document.getElementById("loginModal").style.display = "none";
+}
+
+// Slideshow automático
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+  const slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 4000); // cambia cada 4 segundos
+}
+
+// ----- Control manual (si querés usar los puntos) -----
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("demo");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-white";
+}
